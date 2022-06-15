e2c5dfd0d7e4b019b912314da9bdfccc5522d430
Ben Merritt <BenMerritt@chiefarchitect.com>
2022-06-15 16:22:54 -0700

    Add Oruga

--- vue3/vite.config.ts
+++ vue3/vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['@oruga-ui/oruga-next'],
+  },
   plugins: [vue()],
   resolve: {
     alias: {
