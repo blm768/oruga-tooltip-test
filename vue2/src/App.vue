3c1e4f8ff0d70002f12f95b6e43cd569b870c754
Ben Merritt <BenMerritt@chiefarchitect.com>
2022-06-15 18:02:45 -0700

    Add tooltip testing

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,5 +1,48 @@
 <template>
   <div id="app">
+    <OTooltip
+      id="top-left"
+      label="Top Left Tooltip"
+      :position="position"
+    >
+      Top Left
+    </OTooltip>
+
+    <OTooltip
+      id="top-middle"
+      label="Top Middle Tooltip"
+      :position="position"
+    >
+      Top Middle
+    </OTooltip>
+
+
+    <OTooltip
+      label="Bottom Left Tooltip"
+      id="bottom-left"
+      :position="position"
+    >
+      Bottom Left
+    </OTooltip>
+
+    <OTooltip
+      label="Middle Tooltip"
+      id="middle"
+      :position="position"
+    >
+      Middle
+    </OTooltip>
+
+    <OField label="Position">
+      <OSelect v-model="position">
+        <option value="auto">Auto</option>
+        <option value="top">Top</option>
+        <option value="left">Left</option>
+        <option value="right">Right</option>
+        <option value="bottom">Bottom</option>
+      </OSelect>
+    </OField>
+
   </div>
 </template>
 
@@ -8,9 +51,45 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'App',
+  data() {
+    return {
+      position: 'auto',
+    };
+  },
 });
 </script>
 
 <style>
 @import '@oruga-ui/oruga/dist/oruga.css';
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style scoped>
+#app {
+  height: 100vh;
+  position: relative;
+  width: 100vw;
+}
+
+#top-middle {
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+#bottom-left {
+  bottom: 0;
+  left: 0;
+  position: absolute;
+}
+
+#middle {
+  top: 50%;
+  left: 50%;
+  position: absolute;
+}
 </style>
